70f981203137fe417a723eb454c0d0409d4e6ec3
Hitansh03 <95155668+Hitansh03@users.noreply.github.com>
2021-11-28 10:53:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//localStorage.clear()
+console.log("Inside Script.js");
+console.log(localStorage) 
+if (localStorage.cards){
+    $('.cards').children().remove()
+    $('.cards').append(JSON.parse(localStorage.getItem('cards')))
+}
+
+const setStorage = () => {
+    localStorage.removeItem('cards') // remove all the values
+    localStorage.setItem('cards', JSON.stringify($('.cards').html()))
+}
+
+//add new todo item
+$("form").on("submit", (event) => {
+    event.preventDefault();
+    console.log("Add Clicked!");
+    const title=$("input").val();
+    const card= `
+    <div class="card my-2">
+        <div class="card-block ml-3">
+        <button id="delete" type="button" class="btn btn-danger float-right my-2">Delete</button>
+        <button id="done" type="button" class="btn btn-success float-right my-2 mr-2">Done</button>
+        <p class="card-title lead my-2">${title}</p>
+        </div>
+    </div> `
+    ;
+$(".cards").append(card);
+
+//reset input
+$("form").trigger("reset");
+setStorage();
+});
+
+//delete items
+$('.cards').on('click', '#delete', () => {
+    alert("Are you sure you want to delete?")
+    $(event.target).offsetParent().remove()
+    setStorage();
+})
